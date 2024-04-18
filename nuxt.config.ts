1302bcd287aef0ae95553d6690e97acd05710207
yuhueilee <wendylee70127@gmail.com>
2024-04-18 10:41:20 +0800

    Add alias config for imported elements paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    devtools: { enabled: true },
+    alias: {
+        "@": resolve(__dirname, "/"),
+    },
+});
