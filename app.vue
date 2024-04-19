463d5c795eb9232fa2190c807d69262c17ab494e
yuhueilee <wendylee70127@gmail.com>
2024-04-19 13:30:21 +0800

    Add header for routing pages

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/events">Events</NuxtLink>
+          </li>
+        </ul>
+      </header>
       <NuxtPage />
     </NuxtLayout>
   </div>
